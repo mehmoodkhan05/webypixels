9432351965eddb4380974bcbd10ddd826cae9fd5
YourGitHubUsername <youremail@example.com>
2025-11-11 06:14:34 +0000

    final 1

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,11 +2,11 @@ export const testimonialsData = [
   {
     quote:
       "WebyPixels turned our idea into a fully functional product in record time. The team is incredibly responsive and creative.",
-    author: "Sarah K., Startup Founder",
+    author: "Mohammed Ishfaq - UK",
   },
   {
     quote: "They delivered a custom ERP system that’s now the backbone of our operations. Highly recommend!",
-    author: "Raj P., Business Owner",
+    author: "Sana Ullah - Sanafoods.pk",
   },
 ];
 
